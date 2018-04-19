03c09078270bf9845a03b074f5e1d210eef574cc
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-04-19 03:25:48 +0200

    Fix Modal on mobile

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,28 +5,33 @@ import Modal from 'material-ui/Modal';
 
 const styles = theme => ({
   modal: {
+    overflow: 'scroll'
+  },
+  modalInner: {
     position: 'absolute',
-    maxWidth: theme.spacing.unit * 75,
+    maxWidth: '80%',
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 2,
-    top: '50%',
+    margin: '40px 0',
     left: '50%',
-    transform: 'translate(-50%, -50%)',
+    transform: 'translateX(-50%)',
     borderRadius: theme.spacing.unit,
-    outline: 0
+    outline: 0,
+    overflow: 'scroll'
   }
 });
 
 function CentralizedModal({ open, onClose, children, classes }) {
   return (
     <Modal
+      classes={{ root: classes.modal }}
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
       open={open}
       onClose={onClose}
     >
-      <div className={classes.modal}>{children}</div>
+      <div className={classes.modalInner}>{children}</div>
     </Modal>
   );
 }
